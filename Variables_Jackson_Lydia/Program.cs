5c0613aca6e1dc98a1a3df582251aa1b6a2f1766
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-02-19 15:21:34 -0600

    git

--- Variables_Jackson_Lydia/Program.cs
+++ Variables_Jackson_Lydia/Program.cs
@@ -4,9 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int x; //DECLARATION
-            string playerName; // STRING DECLARATION
-            bool isRunning; //
+            int favoriteNumber;
+            bool isJumping = false, isRunning = false;
+
+            float myFloat;
+            favoriteNumber = 6;
+            myFloat = 66.66f;
+            const double finalGrade = 98.0;
+
+
+
+            Console.WriteLine("Favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floatting #: " + myFloat);
+            Console.WriteLine("Final Grade: " + finalGrade);
+
         }
     }
 }
