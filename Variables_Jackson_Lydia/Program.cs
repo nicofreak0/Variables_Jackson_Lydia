a0745165a7a7f4ea80123c44e30710a8ad51e49f
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-02-20 12:12:03 -0600

    comments

--- Variables_Jackson_Lydia/Program.cs
+++ Variables_Jackson_Lydia/Program.cs
@@ -4,20 +4,26 @@
     {
         static void Main(string[] args)
         {
+            // declares an integer to favorite number
             int favoriteNumber;
+            // booleans initialized false for jumping and running
             bool isJumping = false, isRunning = false;
 
+            // declared random float variable
             float myFloat;
+            // assigning the favoriteNumber to 6
             favoriteNumber = 6;
+            // assigning myFloat to 66.66
             myFloat = 66.66f;
+            // initalizing constant finalGrade to 98
             const double finalGrade = 98.0;
 
 
-
-            Console.WriteLine("Favorite #: " + favoriteNumber);
+            // prints all variables to console
+            Console.WriteLine("Favorite Number: " + favoriteNumber);
             Console.WriteLine("Jumping: " + isJumping);
             Console.WriteLine("Running: " + isRunning);
-            Console.WriteLine("Random Floatting #: " + myFloat);
+            Console.WriteLine("Random Floating Number: " + myFloat);
             Console.WriteLine("Final Grade: " + finalGrade);
 
         }
